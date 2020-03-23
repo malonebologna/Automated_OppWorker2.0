e5eca6d7ad451e4ed07fe4f1a1aee38759ff5e65
Tim Malone <timmalone@FTCCs-MacBook-Pro.local>
2020-03-23 16:56:17 -0400

    commit

--- csv_import.py
+++ csv_import.py
@@ -3,12 +3,12 @@ import csv
 with open('BDSOpportunityAssignment.csv', 'r') as csv_file:
 	csv_reader = csv.DictReader(csv_file)
 
-	for line in csv_reader:
-		print(line['NAICS Code Assigned'])
-		print(line['Additional BDS NAICS codes'])
+	with open('new_names.csv', 'w') as new_file:
+		csv_writer = csv.writer(new_file, delimiter='\n')
 
-	# with open('new_names.csv', 'w') as new_file:
-	# 	csv_writer = csv.writer(new_file, delimiter='\n')
+		for line in csv_reader:
+			csv_writer.writerow(line)
 
-	# 	for line in csv_reader:
-	# 		csv_writer.writerow(line)
+			for line in csv_reader:
+				print(line['NAICS Code Assigned'])
+				print(line['Additional BDS NAICS codes'])
