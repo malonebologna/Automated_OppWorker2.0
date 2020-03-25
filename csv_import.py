9a3ec7356f7ab9139f54410caa7a0c8ca0712681
Tim Malone <timmalone@FTCCs-MacBook-Pro.local>
2020-03-25 14:14:29 -0400

    commit

--- csv_import.py
+++ csv_import.py
@@ -1,8 +1,13 @@
 import csv
 
-csvFile = 'BDSOpportunityAssignment.csv'
-with open(csvFile, 'r') as csv_file:
+orig_csv_file = 'BDSOpportunityAssignment.csv'
+new_csv_file = 'editedOppsList.csv'
+
+with open(orig_csv_file, 'r') as csv_file:
 	csv_reader = csv.DictReader(csv_file)
 
-	for line in csv_reader:
-		print(line)
+	with open(new_csv_file, 'w') as new_file:
+		csv_writer = csv.writer(new_file, delimiter='-')
+
+		for line in csv_reader:
+			csv_writer.writerow(line)
